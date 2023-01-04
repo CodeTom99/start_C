abf60ba70714961e613566296efa6c401879eb1c
zhanghongyuan <502470292@qq.com>
2023-01-04 21:45:09 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <functional>
 #include <list>
+#include <forward_list> 
 using namespace std;
 //STL六大部件
 //容器 分配器 算法 迭代器 适配器 仿函数 
@@ -11,18 +12,7 @@ using namespace std;
 //容器百万级别
 int main()
 {
-	int ia[6] = { 27,210,12,47,109,83 };
-	//注意，分配器这里没有错误检查，前后要对应
-	vector<int, allocator<int>> vi(ia, ia + 6);
-	cout << count_if(vi.begin(), vi.end(), not1(bind2nd(less<int>(), 40)));
-	for (auto elem : vi)
-	{
-		cout << elem << endl;
-	}
-	for (auto& elem : vi)
-	{
-		elem *= 3;
-	}
+	
 	return 0;
 }
   
