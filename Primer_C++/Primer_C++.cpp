5605da2d462af2d6452c1ff22fb528472e05eb1c
zhanghongyuan <502470292@qq.com>
2023-01-04 22:19:50 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -10,9 +10,21 @@ using namespace std;
 //容器 分配器 算法 迭代器 适配器 仿函数 
 //算法：模板函数 例如 push find sort pop
 //容器百万级别
+template <class I,class T>
+void func_impl(I iter, T t)
+{
+	T tmp;
+}
+template<class I>
+inline
+void func(I iter)
+{
+	func_impl(iter, *iter);
+}
 int main()
 {
-	
+	int i = 5;
+	func(&i);
 	return 0;
 }
   
