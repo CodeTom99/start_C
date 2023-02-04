2dd44539b4adbe91469fb07d0c4dd8e5fdf72055
zhanghongyuan <502470292@qq.com>
2023-02-04 13:04:28 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -1,5 +1,6 @@
 ﻿#include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 //Written by CodeTom99
 int main()
@@ -9,5 +10,6 @@ int main()
 	string s3 = "hiya";
 	string s4(10, 'c');
 	cout << s4 << endl;
+	cout << "扬帆起航" << endl;
 	return 0;
 }
