2d873176fb23182e0cdff6756f700ae166013994
zhanghongyuan <502470292@qq.com>
2023-02-04 13:43:35 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -1,10 +1,15 @@
 ﻿#include<iostream>
 #include<string>
 #include<vector>
+#include<memory>
+#include<list>
 using namespace std;
 //Written by CodeTom99
 int main()
 {
-	
+	shared_ptr<string> p1;
+	shared_ptr<list<int>> p2;
+	if (p1 && p1->empty())
+		*p1 = "hi";
 	return 0;
 }
