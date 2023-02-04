b48de5e46ce534720bfa1d82f45938ece6f959ed
zhanghongyuan <502470292@qq.com>
2023-02-04 14:18:24 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -11,5 +11,12 @@ int main()
 	shared_ptr<list<int>> p2;
 	if (p1 && p1->empty())
 		*p1 = "hi";
+	shared_ptr<int> p3 = make_shared<int>(42);
+	shared_ptr<string> p4 = make_shared<string>(10, '9');
+	shared_ptr<int> p5 = make_shared<int>();
+	auto p6 = make_shared<vector<string>>();
+	p6->push_back("666");
+	auto it = p6->begin();
+	cout << *it << endl;
 	return 0;
 }
