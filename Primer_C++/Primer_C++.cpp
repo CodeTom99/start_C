84046ad84cd5482ad0fa877f089676884f26e566
zhanghongyuan <502470292@qq.com>
2023-02-04 13:22:03 +0800

    1.1

--- Primer_C++/Primer_C++.cpp
+++ Primer_C++/Primer_C++.cpp
@@ -5,11 +5,6 @@ using namespace std;
 //Written by CodeTom99
 int main()
 {
-	string s1;
-	string s2 = s1;
-	string s3 = "hiya";
-	string s4(10, 'c');
-	cout << s4 << endl;
-	cout << "扬帆起航" << endl;
+	
 	return 0;
 }
